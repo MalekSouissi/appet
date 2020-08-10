5a544ba873e13ce58b0533dc8ad8e43bc4670db6
Dell <malekSouissi>
2020-08-10 20:05:11 +0200

    Initial commit

---
+++ lib/ui/shared/app_colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/cupertino.dart';
+
+const Color darkMove = Color(0xff8909A3);
+const Color lightMove = Color(0xffCA1BEF);
+const Color lightGreen = Color(0xff8909A3);
---
+++ lib/ui/shared/ui_helpers.dart
@@ -0,0 +1,2 @@
+// dimensions elli yetaawdou fl app kima height w l width w sizeboxex w l paddings XD
+// ayy haja teb3a spacing
